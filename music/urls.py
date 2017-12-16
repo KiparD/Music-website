2480ac84934e0135ae7d8b08bcc6a769833a1cd5
Denis Kiparenko <Kiparenkoden@gmail.com>
2017-12-17 00:25:48 +0200

    User Model and Creating Accounts

--- music/urls.py
+++ music/urls.py
@@ -5,6 +5,10 @@ app_name = 'music'
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+
+    # /music/2/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
     # /music/album/add/

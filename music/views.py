b3c0e22183dca7ee4ac8d6717c07c2b24935af8c
Denis Kiparenko <Kiparenkoden@gmail.com>
2017-12-28 11:46:39 +0200

    Create_album, base, urls

--- music/views.py
+++ music/views.py
@@ -1,72 +1,36 @@
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate, login
-from django.views import generic
-from django.views.generic import View
-from .models import Album
-from .forms import UserForm
+from django.contrib.auth import logout
+from django.http import JsonResponse
+from django.db.models import Q
+from .models import Album, Song
+from .forms import UserForm, AlbumForm, SongForm
+
+AUDIO_FILE_TYPES = ['wav', 'mp3', 'ogg']
+IMAGE_FILE_TYPES = ['png', 'jpg', 'jpeg']
 
 
-class IndexView(generic.ListView):
-    template_name = 'music/index.html'
-    context_object_name = 'all_albums'
-
-    def get_queryset(self):
-        return Album.objects.all()
-
-
-class DetailView(generic.DetailView):
-    model = Album
-    template_name = 'music/detail.html'
-
-
-class AlbumCreate(CreateView):
-    model = Album
-    fields = ['artist', 'album_title', 'genre', 'album_logo']
-
-
-class AlbumUpdate(UpdateView):
-    model = Album
-    fields = ['artist', 'album_title', 'genre', 'album_logo']
-
-
-class AlbumDelete(DeleteView):
-    model = Album
-    success_url = reverse_lazy('music:index')
-
-
-class UserFormView(View):
-    form_class = UserForm
-    template_name = 'music/registration_form.html'
-
-    # display blank form
-    def get(self, request):
-        form = self.form_class(None)
-        return render(request, self.template_name, {'form': form})
-
-    # process form data
-    def post(self, request):
-        form = self.form_class(request.POST)
-
+def create_album(request):
+    if not request.user.is_authenticated():
+        return render(request, 'music/login.html')
+    else:
+        form = AlbumForm(request.POST or None, request.FILES or None)
         if form.is_valid():
-
-            user = form.save(commit=False)
-
-            # cleaned (normalized) data
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user.set_password(password)
-            user.save()
-
-            # returns User objects if credentials are correct
-            user = authenticate(username=username, password=password)
-
-            if user is not None:
-
-                if user.is_active:
-                    login(request, user)
-                    return redirect('music:index')
-
-        return render(request, self.template_name, {'form': form})
-
+            album = form.save(commit=False)
+            album.user = request.user
+            album.album_logo = request.FILES['album_logo']
+            file_type = album.album_logo.url.split('.')[-1]
+            file_type = file_type.lower()
+            if file_type not in IMAGE_FILE_TYPES:
+                context = {
+                    'album': album,
+                    'form': form,
+                    'error_message': 'Image file must be PNG, JPG, or JPEG',
+                }
+                return render(request, 'music/create_album.html', context)
+            album.save()
+            return render(request, 'music/detail.html', {'album': album})
+        context = {
+            'form': form,
+        }
+        return render(request, 'music/create_album.html', context)

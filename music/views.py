661633798a293d0a9663279136a60b980a8046d3
Denis Kiparenko <Kiparenkoden@gmail.com>
2017-12-29 12:53:59 +0200

    Views

--- music/views.py
+++ music/views.py
@@ -71,6 +71,20 @@ def create_song(request, album_id):
     return render(request, 'music/create_song.html', context)
 
 
+def delete_album(request, album_id):
+    album = Album.objects.get(pk=album_id)
+    album.delete()
+    albums = Album.objects.filter(user=request.user)
+    return render(request, 'music/index.html', {'albums': albums})
+
+
+def delete_song(request, album_id, song_id):
+    album = get_object_or_404(Album, pk=album_id)
+    song = Song.objects.get(pk=song_id)
+    song.delete()
+    return render(request, 'music/detail.html', {'album': album})
+
+
 def detail(request, album_id):
     if not request.user.is_authenticated():
         return render(request, 'music/login.html')
@@ -78,6 +92,35 @@ def detail(request, album_id):
         user = request.user
         album = get_object_or_404(Album, pk=album_id)
         return render(request, 'music/detail.html', {'album': album, 'user': user})
+
+
+def favorite(request, song_id):
+    song = get_object_or_404(Song, pk=song_id)
+    try:
+        if song.is_favorite:
+            song.is_favorite = False
+        else:
+            song.is_favorite = True
+        song.save()
+    except (KeyError, Song.DoesNotExist):
+        return JsonResponse({'success': False})
+    else:
+        return JsonResponse({'success': True})
+
+
+def favorite_album(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        if album.is_favorite:
+            album.is_favorite = False
+        else:
+            album.is_favorite = True
+        album.save()
+    except (KeyError, Album.DoesNotExist):
+        return JsonResponse({'success': False})
+    else:
+        return JsonResponse({'success': True})
+
 
 def index(request):
     if not request.user.is_authenticated():
@@ -99,3 +142,67 @@ def index(request):
             return render(request, 'music/index.html', {'albums': albums})
 
 
+def logout_user(request):
+    logout(request)
+    form = UserForm(request.POST or None)
+    context = {
+        'form': form,
+    }
+    return render(request, 'music/login.html', context)
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                albums = Album.objects.filter(user=request.user)
+                return render(request, 'music/index.html', {'albums': albums})
+            else:
+                return render(request, 'music/login.html', {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, 'music/login.html', {'error_message': 'Invalid login'})
+    return render(request, 'music/login.html')
+
+
+def register(request):
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user.set_password(password)
+        user.save()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                albums = Album.objects.filter(user=request.user)
+                return render(request, 'music/index.html', {'albums': albums})
+    context = {
+        'form': form,
+    }
+    return render(request, 'music/register.html', context)
+
+
+def songs(request, filter_by):
+    if not request.user.is_authenticated():
+        return render(request, 'music/login.html')
+    else:
+        try:
+            song_ids = []
+            for album in Album.objects.filter(user=request.user):
+                for song in album.song_set.all():
+                    song_ids.append(song.pk)
+            users_songs = Song.objects.filter(pk__in=song_ids)
+            if filter_by == 'favorites':
+                users_songs = users_songs.filter(is_favorite=True)
+        except Album.DoesNotExist:
+            users_songs = []
+        return render(request, 'music/songs.html', {
+            'song_list': users_songs,
+            'filter_by': filter_by,
+        })
